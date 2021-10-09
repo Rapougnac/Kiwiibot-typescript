22e1db59be48cff1c3b9cce00de767ad545d5d39
Rapougnac <74512338+Rapougnac@users.noreply.github.com>
2021-10-09 22:34:27 +0200

    Fixed interface of Interaction.ts

--- src/struct/interfaces/Interaction.ts
+++ src/struct/interfaces/Interaction.ts
@@ -1,27 +1,50 @@
-import { User, GuildMember } from 'discord.js';  
-
+import { User, GuildMember, Snowflake, Channel, Role } from 'discord.js';
+import CommandInteraction from '../Interactions/CommandInteraction';
+import CommandInteractionOptionResolver from '../Interactions/CommandInteractionOptionResolver';
+/**
+ * The sub-options of the options
+ */
 export interface SubOptions {
+    /**
+     * The name of the option
+     */
     name: string;
+    /**
+     * The type of the option, `string` is caused by {@link CommandInteraction.transformOption}
+     */
     type: number | string;
     value: string;
-};
+}
 
+/**
+ * The options of an {@link CommandInteraction}
+ */
 export interface Options {
+    /**
+     * The name of the option
+     */
     name: string;
+    /**
+     * The type of the option
+     */
     type: number;
+    /**
+     * If there's reccursive options
+     */
     options: SubOptions[] | Options[];
-};
+    /**
+     * The value of the option
+     */
+    value: string;
+}
 
 export interface Data {
     name: string;
-    id: string;
+    id: Snowflake;
     content?: string;
     options?: Options[];
-    resolved?: {
-        user: User;
-        member: GuildMember;
-    };
-};
+    resolved?: ResolvedData;
+}
 export interface Member {
     avatar?: string;
     deaf: boolean;
@@ -33,23 +56,36 @@ export interface Member {
     premium_since: string;
     roles: Array<string>;
     user: User;
-};
+}
 export interface InteractionUser {
     avatar: string;
     discriminator: string;
-    id: string;
+    id: Snowflake;
     public_flags: number;
     username: string;
-};
+}
 export interface Interaction {
     member?: Member;
     user?: InteractionUser;
-    guild_id?: string;
+    guild_id?: Snowflake;
     data: Data;
     token: string;
     type: number;
     version: number;
-    application_id: string;
-    channel_id: string;
-    id: string;
-};
+    application_id: Snowflake;
+    channel_id: Snowflake;
+    id: Snowflake;
+}
+/**
+ * The data resolved
+ */
+export interface ResolvedData {
+    /**
+     * The user
+     */
+    user?: User;
+    /**
+     * The guild member
+     */
+    member?: GuildMember;
+}
