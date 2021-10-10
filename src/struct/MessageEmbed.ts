314ae6e256cfb42fdd094da206c3d59e94a7d156
Rapougnac <74512338+Rapougnac@users.noreply.github.com>
2021-10-10 18:35:39 +0200

    Create MessageEmbed.ts

---
+++ src/struct/MessageEmbed.ts
@@ -0,0 +1,17 @@
+import { MessageEmbed } from 'discord.js';
+
+/**
+ * An extended version of the `MessageEmbed`
+ * @extends MessageEmbed
+ */
+export default class ExtendedMessageEmbed extends MessageEmbed {
+    /**
+     * @param inline The inline of the embed, leave it empty will return `false`
+     */
+    addBlankField(inline: boolean = false): this {
+        // Handling errors
+        if (typeof inline !== 'boolean')
+            throw new TypeError('The inline cannot be other than a boolean');
+        return super.addField('\u200b', '\u200b', inline);
+    }
+};
