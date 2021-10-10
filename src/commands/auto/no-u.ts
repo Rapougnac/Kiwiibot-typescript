e7e3a020ba87ee798509c0781cb2afd0a2d9e6e1
Rapougnac <74512338+Rapougnac@users.noreply.github.com>
2021-10-10 19:14:01 +0200

    Create no-u.ts

---
+++ src/commands/auto/no-u.ts
@@ -0,0 +1,16 @@
+import KiwiiClient from "../../struct/Client";
+import Command from "../../struct/Command";
+
+export default class AutoNOU extends Command {
+    constructor(client: KiwiiClient) {
+        super(client, {
+            name: 'no-u',
+            aliases: ['no-you'],
+            description: 'no u',
+            category: 'auto',
+        });
+    }
+    public execute() {
+        this.message!.channel.send('no u');
+    }
+}
