812f5187ac76df4fea4e8dc28fb9eb7f1e9e06e9
Rapougnac <74512338+Rapougnac@users.noreply.github.com>
2021-10-11 16:01:08 +0200

    Added deps + 3000y.ts

---
+++ src/commands/edit-images/amethyste/3000y.ts
@@ -0,0 +1,59 @@
+//@ts-ignore
+import * as AmeClient from 'amethyste-api';
+import { Message, MessageEmbed, MessageAttachment, GuildMember } from 'discord.js';
+import Command from '../../../struct/Command';
+import Client from '../../../struct/Client';
+export default class ThreeTousandYearsCommand extends Command {
+  constructor(client: Client) {
+    super(client, {
+      name: '3000y',
+      aliases: [],
+      description: 'Sends you, or the specified member with the 3000y meme',
+      category: 'image-manipulation',
+      cooldown: 5,
+      utilisation: '{prefix}3000y <member>',
+    });
+  }
+  async execute(client: Client, message: Message, args: string[]) {
+    const AmeAPI = new AmeClient(client.config.amethyste.client);
+    if(message.guild){
+      message.channel.startTyping();
+      let member =
+      message.mentions.members!.first() ||
+      message.guild.members.cache.get(args[0]) ||
+      message.guild.members.cache.find(
+        (r) =>
+          r.user.username
+            .toLowerCase()
+            .startsWith(args.join(' ').toLowerCase()) ||
+          r.user.username.toLowerCase().endsWith(args.join(' ').toLowerCase())
+      ) ||
+      message.guild.members.cache.find(
+        (r) =>
+          r.displayName
+            .toLowerCase()
+            .startsWith(args.join(' ').toLowerCase()) ||
+          r.displayName.toLowerCase().endsWith(args.join(' ').toLowerCase())
+      );
+      if (args.length <= 0) member = message.member as GuildMember | undefined;
+      let m = await message.channel.send(message.guild.i18n.__mf("common.wait"));
+      const buffer = await AmeAPI.generate('3000years', {
+        url: member!.user.displayAvatarURL({ format: 'png', size: 2048 }),
+      });
+      const attachment = new MessageAttachment(buffer, '3000years.png');
+      m.delete({ timeout: 3000 });
+      message.channel.send(attachment);
+      message.channel.stopTyping();
+    } else {
+      let member = message.author;
+      let m = await message.channel.send(message.guild!.i18n.__mf("common.wait"));
+      const buffer = await AmeAPI.generate('3000years', {
+        url: member.displayAvatarURL({ format: 'png', size: 2048 }),
+      });
+      const attachment = new MessageAttachment(buffer, '3000years.png');
+      m.delete({ timeout: 3000 });
+      message.channel.send(attachment);
+      message.channel.stopTyping();
+    }
+  }
+};
