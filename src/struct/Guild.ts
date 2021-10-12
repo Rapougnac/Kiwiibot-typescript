0cdd4104128d4c68ef2a95875e28810eaee44aef
Rapougnac <74512338+Rapougnac@users.noreply.github.com>
2021-10-12 21:17:41 +0200

    Why i18n ? Why this is this horribly mess D: IT DOESN'T WORK IN ADDITION!

--- src/struct/Guild.ts
+++ src/struct/Guild.ts
@@ -1,19 +1,20 @@
 import { Structures, Guild, Client } from 'discord.js';
-import { I18n, configure } from 'i18n';
+import i18n, { I18n } from 'i18n';
 import KiwiiClient from './Client';
 import * as path from 'path';
 
 class ExtendedGuild extends Guild {
     public readonly i18n: I18n;
     public prefix: string;
-    constructor(client: KiwiiClient,data: any) {
+    constructor(client: KiwiiClient, data: any) {
         super(client as unknown as Client, data);
-        configure({
+
+        i18n.configure({
             locales: ['en', 'fr'],
             directory: path.join(process.cwd(), 'locales'),
             objectNotation: true,
         });
-        this.i18n = new I18n();
+        this.i18n = i18n;
         this.i18n.setLocale('en');
         this.prefix = client.prefix;
     }
