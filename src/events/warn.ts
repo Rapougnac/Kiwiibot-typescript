8088acf3d8c43220294da7322fbdeaeaeb201398
Rapougnac <74512338+Rapougnac@users.noreply.github.com>
2021-10-12 08:42:57 +0200

    Added warn event

---
+++ src/events/warn.ts
@@ -0,0 +1,15 @@
+import KiwiiClient from '../struct/Client';
+import Event from '../struct/Event';
+
+
+export default class WarnEvent extends Event {
+    constructor(client: KiwiiClient) {
+        super(client, {
+            name: 'warn',
+            once: false,
+        });
+    }
+    public execute(info: string) {
+        console.warn(info);
+    }
+}
