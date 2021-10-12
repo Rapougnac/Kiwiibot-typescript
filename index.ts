0b7ecabd10a4e6f4cbaaa676cad7157188a7ec6d
Rapougnac <74512338+Rapougnac@users.noreply.github.com>
2021-10-12 14:36:40 +0200

    Changed inedx.ts

---
+++ index.ts
@@ -0,0 +1,10 @@
+import KiwiiClient from './src/struct/Client';
+import * as config from './config';
+
+const client = new KiwiiClient({
+    //@ts-ignore
+    config: config,
+    owners: '253554702858452992',
+});
+
+client.start();
