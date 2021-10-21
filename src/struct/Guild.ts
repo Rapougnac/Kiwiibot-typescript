6cedcfaa573157edef9f8e55457f0dad90d59f41
Rapougnac <74512338+Rapougnac@users.noreply.github.com>
2021-10-21 13:37:57 +0200

    i18n..

--- src/struct/Guild.ts
+++ src/struct/Guild.ts
@@ -8,13 +8,12 @@ class ExtendedGuild extends Guild {
     public prefix: string;
     constructor(client: KiwiiClient, data: any) {
         super(client as unknown as Client, data);
-
-        i18n.configure({
+        this.i18n = new I18n();
+        this.i18n.configure({
             locales: ['en', 'fr'],
             directory: path.join(process.cwd(), 'locales'),
             objectNotation: true,
         });
-        this.i18n = i18n;
         this.i18n.setLocale('en');
         this.prefix = client.prefix;
     }
