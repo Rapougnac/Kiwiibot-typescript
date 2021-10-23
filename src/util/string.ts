22af5ecb776834ac180b2593e90a8620dfe0953d
Rapougnac <74512338+Rapougnac@users.noreply.github.com>
2021-10-23 20:42:08 +0200

    Invert flags

--- src/util/string.ts
+++ src/util/string.ts
@@ -337,7 +337,7 @@ function translatePermissions(
  */
 function upperFirstButAcceptEmojis(str: string): string {
     const reg =
-        /[\u{1f300}-\u{1f5ff}\u{1f900}-\u{1f9ff}\u{1f600}-\u{1f64f}\u{1f680}-\u{1f6ff}\u{2600}-\u{26ff}\u{2700}-\u{27bf}\u{1f1e6}-\u{1f1ff}\u{1f191}-\u{1f251}\u{1f004}\u{1f0cf}\u{1f170}-\u{1f171}\u{1f17e}-\u{1f17f}\u{1f18e}\u{3030}\u{2b50}\u{2b55}\u{2934}-\u{2935}\u{2b05}-\u{2b07}\u{2b1b}-\u{2b1c}\u{3297}\u{3299}\u{303d}\u{00a9}\u{00ae}\u{2122}\u{23f3}\u{24c2}\u{23e9}-\u{23ef}\u{25b6}\u{23f8}-\u{23fa}\u{200d}\u{2139}] /ug;
+        /[\u{1f300}-\u{1f5ff}\u{1f900}-\u{1f9ff}\u{1f600}-\u{1f64f}\u{1f680}-\u{1f6ff}\u{2600}-\u{26ff}\u{2700}-\u{27bf}\u{1f1e6}-\u{1f1ff}\u{1f191}-\u{1f251}\u{1f004}\u{1f0cf}\u{1f170}-\u{1f171}\u{1f17e}-\u{1f17f}\u{1f18e}\u{3030}\u{2b50}\u{2b55}\u{2934}-\u{2935}\u{2b05}-\u{2b07}\u{2b1b}-\u{2b1c}\u{3297}\u{3299}\u{303d}\u{00a9}\u{00ae}\u{2122}\u{23f3}\u{24c2}\u{23e9}-\u{23ef}\u{25b6}\u{23f8}-\u{23fa}\u{200d}\u{2139}] /gu;
 
     const emoji = reg.exec(str);
     if (!emoji) {
@@ -348,6 +348,23 @@ function upperFirstButAcceptEmojis(str: string): string {
         str = emoji[0] + str;
     }
     return str;
+}
+
+function cleanQuery(args: string[] | string, match: string = '_'): string[] {
+    let joinedArr, rest;
+    if (Array.isArray(args)) {
+        joinedArr = args.join(match);
+    }
+    if (joinedArr) {
+        const i = joinedArr.indexOf(match);
+        rest = [joinedArr.slice(i + 1), joinedArr.slice(0, i)];
+        cleanQuery(rest);
+    } else {
+        const i = args.indexOf(match);
+        rest = [args.slice(i + 1), args.slice(0, i)];
+        cleanQuery(rest as any);
+    }
+    return rest as any;
 }
 
 export {
@@ -363,4 +380,5 @@ export {
     remove,
     translatePermissions,
     upperFirstButAcceptEmojis,
+    cleanQuery,
 };
