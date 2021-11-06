e00492c8bf1ffdb99e345dec80bbf80e814423cc
Rapougnac <74512338+Rapougnac@users.noreply.github.com>
2021-11-06 19:05:29 +0100

    :sparkles: Added cry.ts command

---
+++ src/commands/interactions/cry.ts
@@ -0,0 +1,28 @@
+import { Message, MessageEmbed } from 'discord.js';
+import Command from '../../struct/Command';
+import Client from '../../struct/Client';
+import { gifu } from 'gifu';
+
+export default class CryCommand extends Command {
+    constructor(client: Client) {
+        super(client, {
+            name: 'cry',
+            description: 'Cry',
+            category: 'interactions',
+            utilisation: 'cry',
+        });
+    }
+
+    public execute(client: Client, message: Message, args: string[]) {
+        const gif = gifu('cry');
+        const embed = new MessageEmbed()
+            .setColor('#202225')
+            .setImage(gif)
+            .setTitle(
+                message.guild!.i18n.__mf('cry.msg', {
+                    author: message.author.tag,
+                })
+            );
+        message.channel.send(embed);
+    }
+}
