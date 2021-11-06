db61bf0aab4d9c6df64d74bd2e87bd5049b7f0d2
Rapougnac <74512338+Rapougnac@users.noreply.github.com>
2021-11-06 19:25:33 +0100

    :sparkles: Added anal.ts command

---
+++ src/commands/nsfw/anal.ts
@@ -0,0 +1,35 @@
+import { Message, MessageEmbed } from 'discord.js';
+import Command from '../../struct/Command';
+import Client from '../../struct/Client';
+
+export default class AnalCommand extends Command {
+    constructor(client: Client) {
+        super(client, {
+            name: 'anal',
+            description: 'Sends a random anal image/gif',
+            category: 'nsfw',
+            utilisation: '{prefix}anal',
+            cooldown: 5,
+            nsfw: true,
+            clientPermissions: ['EMBED_LINKS'],
+        });
+    }
+
+    public async execute(
+        client: Client,
+        message: Message,
+        args: string[]
+    ): Promise<Message> {
+        const anal = await this.client.utils.neko.nsfw.anal();
+        const embed = new MessageEmbed()
+            .setColor('#202225')
+            .setImage(anal.url)
+            .setFooter(
+                message.guild!.i18n.__mf('anal.msg', {
+                    author: message.author.tag,
+                }),
+                message.author.displayAvatarURL()
+            );
+        return message.channel.send(embed);
+    }
+}
