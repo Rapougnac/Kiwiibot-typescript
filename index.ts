79ebd07f73565979e63babe4ec0f1331ed0c74f5
Rapougnac <74512338+Rapougnac@users.noreply.github.com>
2021-11-08 21:41:44 +0100

    Removed unused code, + added intents (v13 requires it)

--- index.ts
+++ index.ts
@@ -1,42 +1,19 @@
 import KiwiiClient from './src/struct/Client';
 import * as config from './config';
-import './src/struct/Message';
 import './src/struct/Guild';
-import './src/struct/GuildMember';
-import './src/struct/User';
-import { WSEventType as WSEventTypeOld } from 'discord.js';
-import Interaction from './src/struct/Interactions/Interaction';
-import CommandInteraction from './src/struct/Interactions/CommandInteraction';
-
-type WSEventType = 'INTERACTION_CREATE' | WSEventTypeOld;
+import Intents from './src/struct/Intents';
 
 const client = new KiwiiClient({
     config: config,
     owners: '253554702858452992',
     prefix: 'm?',
+    clientOptions: {
+        intents: Intents.ALL,
+        allowedMentions: {
+            repliedUser: false,
+        }
+    },
 });
 
 client.connect();
 client.start();
-
-client.ws.on('INTERACTION_CREATE' as WSEventType as any, (int: any) => {
-    let interaction = new Interaction(client, int);
-    if (interaction.isCommand()) {
-        const interaction = new CommandInteraction(client, int);
-        const { commandName: name, options } = interaction;
-        const { args } = options;
-        if (!client.slashs.has(name)) return;
-        try {
-            client.slashs.get(name)?.execute(interaction, client, args);
-        } catch (e: any) {
-            const { message, stack } = e;
-            console.error(`Error from command ${name}: ${message}\n${stack}`);
-            interaction.send(
-                'Sorry there was an error executing that command!',
-                {
-                    ephemeral: true,
-                }
-            );
-        }
-    }
-});
