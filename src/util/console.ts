1c726ba1d07a1e51f755141edfdaa71941557961
Rapougnac <74512338+Rapougnac@users.noreply.github.com>
2021-11-18 11:13:06 +0100

    Added table function to console.ts

--- src/util/console.ts
+++ src/util/console.ts
@@ -1,3 +1,6 @@
+import { Console } from 'console';
+import { Transform } from 'stream';
+
 /**
  * Logs on console with color green
  * @param message The message to log on console
@@ -23,4 +26,27 @@ const warn = (message: string, title: string = 'WARN!'): void =>
 const error = (message: string, title: string = ''): void =>
     console.log(title, '\x1b[31mERR!\x1b[0m', message);
 
-export { success, warn, error };
+const table = (input: any[]): void => {
+    const ts = new Transform({
+        transform(chunk, _, cb) {
+            cb(null, chunk);
+        },
+    });
+    const logger = new Console({ stdout: ts, stderr: ts });
+    logger.table(input);
+    const _table = ts.read().toString();
+    let result = '';
+    for (const row of _table.split(/[\r\n]+/)) {
+        let r = row.replace(/[^┬]*┬/, '┌');
+        r = r.replace(/^├─*┼/, '├');
+        r = r.replace(/│[^│]*/, '');
+        r = r.replace(/^└─*┴/, '└');
+        r = r.replace(/'/g, ' ');
+
+        result += `${r}\n`;
+    }
+
+    console.log(result);
+};
+
+export { success, warn, error, table };
