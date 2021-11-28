dd33c48657921c02df70cb2ed5f53ed68dff4f89
Rapougnac <74512338+Rapougnac@users.noreply.github.com>
2021-11-28 17:34:50 +0100

    Fix avatar on dm

--- src/commands/infos/avatar.ts
+++ src/commands/infos/avatar.ts
@@ -1,4 +1,4 @@
-import { Message, MessageEmbed, GuildMember } from 'discord.js';
+import { Message, MessageEmbed, GuildMember, User } from 'discord.js';
 import Command from '../../struct/Command';
 import Client from '../../struct/Client';
 export default class AvatarCommand extends Command {
@@ -15,60 +15,124 @@ export default class AvatarCommand extends Command {
         });
     }
     public async execute(_client: Client, message: Message, args: string[]) {
-        if (!message.guild) return;
-        let member =
-            message.mentions.members!.first() ||
-            message.guild.members.cache.get(args[0]!) ||
-            message.guild.members.cache.find(
-                (r) =>
-                    r.user.username
-                        .toLowerCase()
-                        .startsWith(args.join(' ').toLowerCase()) ||
-                    r.user.username
-                        .toLowerCase()
-                        .endsWith(args.join(' ').toLowerCase())
-            ) ||
-            message.guild.members.cache.find(
-                (r) =>
-                    r.displayName
-                        .toLowerCase()
-                        .startsWith(args.join(' ').toLowerCase()) ||
-                    r.displayName
-                        .toLowerCase()
-                        .endsWith(args.join(' ').toLowerCase())
-            ) ||
-            message.guild.members.cache.find((r) =>
-                r.user.username.toLowerCase().includes(args.join(' '))
-            );
-        if (args.length <= 0) member = message.member as GuildMember;
+        let member: GuildMember | User;
+        if (message.channel.type === 'DM') {
+            message.author = await this.client.users.fetch(message.author, {
+                force: true,
+            });
+            member = message.author;
+        } else {
+            //@ts-expect-error
+            member =
+                message.mentions.members!.first() ||
+                message.guild?.members.cache.get(args[0]!) ||
+                message.guild?.members.cache.find(
+                    (r) =>
+                        r.user.username
+                            .toLowerCase()
+                            .startsWith(args.join(' ').toLowerCase()) ||
+                        r.user.username
+                            .toLowerCase()
+                            .endsWith(args.join(' ').toLowerCase())
+                ) ||
+                message.guild?.members.cache.find(
+                    (r) =>
+                        r.displayName
+                            .toLowerCase()
+                            .startsWith(args.join(' ').toLowerCase()) ||
+                        r.displayName
+                            .toLowerCase()
+                            .endsWith(args.join(' ').toLowerCase())
+                ) ||
+                message.guild?.members.cache.find((r) =>
+                    r.user.username.toLowerCase().includes(args.join(' '))
+                );
+
+            if (args.length <= 0) member = message.member as GuildMember;
+        }
         if (!member) return;
+
         const embed = new MessageEmbed()
             .setAuthor(
-                `${message.guild.i18n.__mf('avatar.avatar_of', {
-                    avatar: member.user.username,
+                `${message.guild?.i18n.__mf('avatar.avatar_of', {
+                    avatar:
+                        member instanceof GuildMember
+                            ? member.user.username
+                            : member.username,
                 })}`
             )
             .setDescription(
-                `${message.guild.i18n.__mf(
-                    'avatar.msg'
-                )}(${member.user.displayAvatarURL({
-                    size: 4096,
-                    dynamic: true,
-                    format: 'png',
-                })})\n\nFormat: [webp](${member.user.displayAvatarURL({
-                    size: 4096,
-                })}) • [jpg](${member.user.displayAvatarURL({
-                    format: 'jpg',
-                    size: 4096,
-                })}) • [jpeg](${member.user.displayAvatarURL({
-                    format: 'jpeg',
-                    size: 4096,
-                })}) • [png](${member.user.displayAvatarURL({
-                    format: 'png',
-                    size: 4096,
-                })}) ${
-                    member.user.avatar!.startsWith('a_')
-                        ? ` • [gif](${member.user.displayAvatarURL({
+                `${message.guild?.i18n.__mf('avatar.msg')}(${
+                    member instanceof GuildMember
+                        ? member.user.displayAvatarURL({
+                              size: 4096,
+                              dynamic: true,
+                              format: 'png',
+                          })
+                        : member.displayAvatarURL({
+                              size: 4096,
+                              dynamic: true,
+                              format: 'png',
+                          })
+                })\n\nFormat: [webp](${
+                    member instanceof GuildMember
+                        ? member.user.displayAvatarURL({
+                              size: 4096,
+                              dynamic: true,
+                              format: 'webp',
+                          })
+                        : member.displayAvatarURL({
+                              size: 4096,
+                              dynamic: true,
+                              format: 'webp',
+                          })
+                }) • [jpg](${
+                    member instanceof GuildMember
+                        ? member.user.displayAvatarURL({
+                              size: 4096,
+                              dynamic: true,
+                              format: 'webp',
+                          })
+                        : member.displayAvatarURL({
+                              size: 4096,
+                              dynamic: true,
+                              format: 'webp',
+                          })
+                }) • [jpeg](${
+                    member instanceof GuildMember
+                        ? member.user.displayAvatarURL({
+                              size: 4096,
+                              dynamic: true,
+                              format: 'jpeg',
+                          })
+                        : member.displayAvatarURL({
+                              size: 4096,
+                              dynamic: true,
+                              format: 'jpeg',
+                          })
+                }) • [png](${
+                    member instanceof GuildMember
+                        ? member.user.displayAvatarURL({
+                              size: 4096,
+                              dynamic: true,
+                              format: 'png',
+                          })
+                        : member.displayAvatarURL({
+                              size: 4096,
+                              dynamic: true,
+                              format: 'png',
+                          })
+                }) ${
+                    member instanceof GuildMember
+                        ? member.user.avatar!.startsWith('a_')
+                            ? ` • [gif](${member.user.displayAvatarURL({
+                                  dynamic: true,
+                                  format: 'gif',
+                                  size: 4096,
+                              })})`
+                            : ''
+                        : member.avatar!.startsWith('a_')
+                        ? ` • [gif](${member.displayAvatarURL({
                               dynamic: true,
                               format: 'gif',
                               size: 4096,
@@ -77,13 +141,19 @@ export default class AvatarCommand extends Command {
                 }`
             )
             .setImage(
-                member.user.displayAvatarURL({
-                    size: 4096,
-                    dynamic: true,
-                    format: 'png',
-                })
+                member instanceof GuildMember
+                    ? member.user.displayAvatarURL({
+                          size: 4096,
+                          dynamic: true,
+                          format: 'png',
+                      })
+                    : member.displayAvatarURL({
+                          size: 4096,
+                          dynamic: true,
+                          format: 'png',
+                      })
             )
-            .setColor(member.displayHexColor || 'GREY');
+            .setColor(member instanceof GuildMember ? member.displayHexColor : member.hexAccentColor || 'GREY');
         message.channel.send({ embeds: [embed] });
     }
 }
