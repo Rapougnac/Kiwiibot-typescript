1e87551d5c7f0656ad849f940cfa920fcb48fa9e
Rapougnac <74512338+Rapougnac@users.noreply.github.com>
2021-12-13 18:44:12 +0100

    I18n was the same everywhere (fixes #9)

--- index.ts
+++ index.ts
@@ -1,6 +1,5 @@
 import KiwiiClient from './src/struct/Client';
 import * as config from './config';
-import './src/struct/Guild';
 import './src/struct/User';
 import Intents from './src/struct/Intents';
 import { error } from './src/util/console';
--- load.ts
+++ load.ts
@@ -1,64 +1,50 @@
 import KiwiiClient from './src/struct/Client';
 import { error } from './src/util/console';
+import { I18n } from 'i18n';
+import * as path from 'path';
 
 export const load = async (client: KiwiiClient) => {
     try {
-        // await checkIfDbExists(client);
-        // const asyncResults = [];
-        // const guilds = [...client.guilds.cache.values()];
+        const asyncResults = [];
         for (const [id, guild] of client.guilds.cache) {
+            const i18n = new I18n();
+            i18n.configure({
+                locales: ['en', 'fr', 'de'],
+                directory: path.join(process.cwd(), 'locales'),
+                defaultLocale: 'en',
+                objectNotation: true,
+            });
+            i18n.setLocale('en');
+            Object.defineProperty(guild, 'i18n', {
+                value: i18n,
+            });
             if (!client.mySql.connected) return guild.i18n.setLocale('en');
-            // eslint-disable-next-line no-await-in-loop
-            const results = (await client.mySql.connection.query(
+            const results = client.mySql.connection.query(
                 `SELECT * FROM guildsettings WHERE guildId = ${id}`
-            )) as unknown as {
+            ) as unknown as {
                 language: string;
                 prefix: string;
+                guildId: string;
             }[][];
-            // asyncResults.push(results);
-            guild.i18n.setLocale(
-                results[0]?.[0]?.language ? results[0][0].language : 'en'
-            );
+            asyncResults.push(results);
         }
-        // const results = await Promise.all(asyncResults);
-        // for (const [i, _res] of results.entries()) {
-        //     // @ts-expect-error: TypeScript doesn't know about the `guildId` column
-        //     const result = _res[0][0];
-        //     const _guild = guilds[i];
-        //     _guild?.i18n.setLocale('en');
-        //     if (!result) {
-        //         // _guild?.i18n.setLocale('en');
-        //         continue;
-        //     }
-        //     const guild = client.guilds.cache.get(result.guildId);
-        //     if (guild && result.guildId === guild.id) {
-        //         guild.i18n.setLocale(result.language ? result.language : 'en');
-        //         guild.prefix = result.prefix;
-        //         continue;
-        //     } else {
-        //         error(
-        //             `Guild ${result.guildId} not found, but settings found in database.`
-        //         );
-        //     }
-        // }
+        const results = await Promise.all(asyncResults);
+        for (const [, _res] of results.entries()) {
+            const result = _res[0]?.[0];
+            if (!result) continue;
+
+            const guild = client.guilds.cache.get(result.guildId);
+            if (guild && result.guildId === guild.id) {
+                guild.i18n.setLocale(result.language ? result.language : 'en');
+                guild.prefix = result.prefix;
+                continue;
+            } else {
+                error(
+                    `Guild ${result.guildId} not found, but settings found in database.`
+                );
+            }
+        }
     } catch (e) {
         error(e as string, '[DATABASE]');
     }
 };
-
-// const checkIfDbExists = async (client: KiwiiClient) => {
-//     const exists = await client.mySql.connection.query(
-//         `SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = 'guildsettings'`
-//     );
-//     //@ts-expect-error: TypeScript doesn't know about the `table_name` column
-//     if (exists[0].length === 0) {
-//         await client.mySql.connection.query(
-//             `CREATE TABLE guildsettings (
-//                 guildId CHAR(18) NOT NULL,
-//                 prefix VARCHAR(5) NOT NULL,
-//                 language VARCHAR(255) NOT NULL DEFAULT 'en',
-//                 PRIMARY KEY (guildId)
-//             )`
-//         );
-//     }
-// };
