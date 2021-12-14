947d5ef12a7405fe265917917e2bd7df8de2d974
Rapougnac <74512338+Rapougnac@users.noreply.github.com>
2021-12-14 14:57:13 +0100

    fix(PingCommand): Unknow interaction triggers

--- src/slash_commands/infos/ping.ts
+++ src/slash_commands/infos/ping.ts
@@ -12,17 +12,23 @@ export default class PingCommand extends SlashCommand {
     }
 
     public async execute(interaction: CommandInteraction): Promise<void> {
-        if (interaction.guild) {
-            const message = await interaction.deferReply({ fetchReply: true });
-            if (!(message instanceof Message)) return;
-            const ping =
-                message.createdTimestamp - interaction.createdTimestamp;
-            const reply = message.guild?.i18n.__mf('ping.msg', {
-                pong: 'o'.repeat(Math.min(Math.round(ping / 100), 1500)),
-                ping,
-                heartbeat: this.client.ws.ping,
-            });
-            interaction.editReply(reply ?? '');
+        try {
+            if (interaction.guild) {
+                const message = await interaction.deferReply({
+                    fetchReply: true,
+                });
+                if (!(message instanceof Message)) return;
+                const ping =
+                    message.createdTimestamp - interaction.createdTimestamp;
+                const reply = message.guild?.i18n.__mf('ping.msg', {
+                    pong: 'o'.repeat(Math.min(Math.round(ping / 100), 1500)),
+                    ping,
+                    heartbeat: this.client.ws.ping,
+                });
+                interaction.editReply(reply ?? '');
+            }
+        } catch (e) {
+            //
         }
     }
 }
