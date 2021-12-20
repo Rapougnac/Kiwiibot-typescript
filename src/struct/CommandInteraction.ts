9f1aa3d3c5ab0a68c2b0df761c4a9c6461b27e9a
Rapougnac <74512338+Rapougnac@users.noreply.github.com>
2021-12-20 21:08:49 +0100

    feat(CommandInteraction): Added CommandInteraction#react()

---
+++ src/struct/CommandInteraction.ts
@@ -0,0 +1,25 @@
+import { CommandInteraction, Message } from "discord.js";
+import type { EmojiIdentifierResolvable, MessageReaction } from "discord.js";
+import type { APIMessage } from 'discord-api-types/v9';
+
+CommandInteraction.prototype.react = async function (emoji: EmojiIdentifierResolvable): Promise<MessageReaction> {
+    if (emoji) emoji = this.client.emojis.resolveIdentifier(emoji) as string;
+    if (!emoji) throw new TypeError('Invalid emoji');
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    const msg = await (this.client as unknown as ClientApi).api.webhooks(this.client.user!.id, this.token).messages('@original').get();
+
+    //@ts-expect-error: Message is private.
+    const message: Message = new Message(this.client, msg);
+
+    return message.react(emoji);
+};
+
+interface ClientApi {
+    readonly api: {
+        webhooks(userId: string, token: string): {
+            messages(id: string): {
+                get(): Promise<APIMessage>
+            }
+        }
+    }
+}
