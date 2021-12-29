9b1e3e98166449b910964eb97db27743b689dcf8
Rapougnac <74512338+Rapougnac@users.noreply.github.com>
2021-12-29 16:13:05 +0100

    feat(Profiles): Added comand to set profile and view profile

---
+++ src/commands/profile/profile.ts
@@ -0,0 +1,95 @@
+import Command from '../../struct/Command';
+import type KiwiiClient from '../../struct/Client';
+import { createCanvas, loadImage } from 'canvas';
+import { MessageAttachment } from 'discord.js';
+import type { Message } from 'discord.js';
+import * as path from 'path';
+import Canvas from '../../struct/Canvas';
+import type { Canvas as NodeCanvas } from 'canvas';
+
+export default class ProfileCommand extends Command {
+  constructor(client: KiwiiClient) {
+    super(client, {
+      name: 'profile',
+      description: 'See your profile.',
+      utilisation: '{prefix}profile',
+      category: 'profile',
+      clientPermissions: [
+        'ATTACH_FILES',
+        'EMBED_LINKS',
+        'SEND_MESSAGES',
+        'VIEW_CHANNEL',
+      ],
+    });
+  }
+
+  public override async execute(_: KiwiiClient, message: Message) {
+    const [[bio]] = (await this.client.mySql.connection.query(
+      `SELECT bio FROM usersettings WHERE id = ${message.author.id}`
+    )) as unknown as [
+      [
+        {
+          bio?: string;
+        }?
+      ]
+    ];
+    const bioText = bio?.bio || 'No bio set.';
+    // #region Profile loading
+    const img = await loadImage(
+      path.join(process.cwd(), 'src', 'assets', 'images', 'Profiles', '1.png')
+    );
+    const avatar = await loadImage(
+      message.author.displayAvatarURL({ format: 'png', size: 4096 })
+    );
+    const canvas = createCanvas(img.width, img.height);
+    const ctx = canvas.getContext('2d');
+    ctx.textDrawingMode = 'path';
+    ctx.patternQuality = 'best';
+    ctx.quality = 'best';
+    ctx.filter = 'bilinear';
+    ctx.antialias = 'subpixel';
+    ctx.drawImage(img, 0, 0, img.width, img.height);
+    ctx.save();
+    Canvas.roundImage(ctx, 617, 23, 200, 200, 50);
+    ctx.clip();
+    ctx.drawImage(avatar, 617, 23, 200, 200);
+    ctx.restore();
+    // ctx.fillStyle = 'rgba(0, 0, 0, 0.5)';
+    ctx.fillStyle = 'rgba(42, 51, 85, 0.8)';
+    Canvas.roundRectangle(ctx, 50, 156, 450, 117, 10, true);
+    ctx.fillStyle = 'rgb(255, 255, 255)';
+    ctx.font = '20px Poppins';
+    ctx.fillStyle = '#A0DCEC';
+    const bioArray = bioText.split(/\s+/g);
+    if (bioArray.some((word) => word.length > 33)) {
+      Canvas.wrap(ctx, bioText, 55, 173, 25, 446);
+    } else {
+      Canvas.wordWrap(ctx, bioText, 55, 173, 25, 446);
+    }
+    ctx.fillStyle = 'rgba(42, 51, 85, 0.8)';
+    ctx.font = applyText(canvas, message.author.username);
+    const textWidth = ctx.measureText(message.author.username).width + 5;
+    Canvas.roundRectangle(ctx, 617, 229, Math.floor(textWidth), 47, 10, true);
+    ctx.fillStyle = '#A0DCEC';
+    ctx.fillText(message.author.username, 617, 261);
+    // #endregion
+    const buffer = canvas.toBuffer('image/png');
+    await message.channel.send({
+      files: [new MessageAttachment(buffer, 'profile.png')],
+    });
+  }
+}
+
+const applyText = <Text extends string = string>(
+  canvas: NodeCanvas,
+  text: Text
+): `${number}px Poppins` => {
+  const ctx = canvas.getContext('2d');
+  let fontSize = 40;
+
+  do {
+    ctx.font = `${(fontSize -= 5)}px Poppins`;
+  } while (ctx.measureText(text).width > canvas.width / 4);
+
+  return ctx.font as `${number}px Poppins`;
+};
---
+++ src/commands/profile/setbio.ts
@@ -0,0 +1,61 @@
+import type { Message } from 'discord.js';
+import type { OkPacket } from 'mysql2';
+import type KiwiiClient from '../../struct/Client';
+import Command from '../../struct/Command';
+
+export default class SetProfileCommand extends Command {
+  constructor(client: KiwiiClient) {
+    super(client, {
+      name: 'setbio',
+      description: 'Set your profile banner.',
+      aliases: ['set-bio', 'setbiography'],
+      utilisation: '{prefix}setbio [bio]',
+      category: 'profile',
+      clientPermissions: [
+        'ATTACH_FILES',
+        'EMBED_LINKS',
+        'SEND_MESSAGES',
+        'VIEW_CHANNEL',
+      ],
+    });
+  }
+
+  public override async execute(
+    _client: KiwiiClient,
+    message: Message,
+    args: string[]
+  ) {
+    const newBio = args.join(' ');
+    if (!newBio) return message.channel.send('Please provide a bio.');
+    if (newBio.length > 200) {
+      return message.channel.send(
+        `Your bio is too long. It must be less than 200 characters.`
+      );
+    }
+    const [[oldBio]] = (await this.client.mySql.connection.query(
+      'SELECT bio FROM usersettings WHERE id = ?',
+      [message.author.id]
+    )) as unknown as [
+      [
+        {
+          bio?: string;
+        }?
+      ]
+    ];
+    if (oldBio?.bio === newBio) {
+      return message.channel.send(`Your bio is already set to ${newBio}`);
+    }
+    const data = await this.client.mySql.connection.query(
+      'UPDATE usersettings SET bio = ? WHERE id = ?',
+      [newBio, message.author.id]
+    );
+    if (!(data[0] as OkPacket).changedRows) {
+      await this.client.mySql.connection.query(
+        'INSERT INTO usersettings (id, bio) VALUES (?, ?)',
+        [message.author.id, newBio]
+      );
+    }
+
+    return message.channel.send(`Your bio has been set.`);
+  }
+}
